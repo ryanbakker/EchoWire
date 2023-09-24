49fafa2e8507be3465922db6708cbfb32058b6b7
ryanbakkerNZ <rmbakker2002@gmail.com>
2023-09-25 02:38:14 +1300

    Communities Page

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -111,7 +112,30 @@ const ThreadCard = ({
             </div>
           </div>
         </div>
+
+        {/* Delete Thread */}
+
+        {/* Show comment logo */}
       </div>
+      {!isComment && community && (
+        <Link
+          href={`/communities/${community.id}`}
+          className="mt-5 flex items-center"
+        >
+          <p className="text-subtle-medium text-gray-1">
+            {formatDateString(createdAt)} &nbsp; • &nbsp; {community.name}{" "}
+            Community
+          </p>
+
+          <Image
+            src={community.image}
+            alt={community.name}
+            width={14}
+            height={14}
+            className="ml-1 rounded-full object-cover"
+          />
+        </Link>
+      )}
     </article>
   );
 };
